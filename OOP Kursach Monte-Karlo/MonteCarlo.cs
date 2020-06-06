4fd4afb24f81ba9c8e4928637acfc2f036bcab86
Денис Северин <deniseverin@mail.ru>
2020-06-06 17:39:43 +0300

    correct using derictives

--- OOP Kursach Monte-Karlo/MonteCarlo.cs
+++ OOP Kursach Monte-Karlo/MonteCarlo.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace OOP_Kursach_Monte_Karlo
 {
